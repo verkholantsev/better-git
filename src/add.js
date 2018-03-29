c1bcf9d8efe6c275e448c001c8ec83718c93e71c
Aleksei Verkholantcev <averkholantcev@atlassian.com>
2018-03-30 06:29:27 +1100

    Add `checkSupportedOpts` to `add`

--- src/add.js
+++ src/add.js
@@ -1,11 +1,16 @@
 // @flow
 
 import mapOpts from './map-opts';
+import checkSupportedOpts from './check-supported-opts';
 
 import type { Git } from './git-factory';
 import type { Opts } from './map-opts';
 
+const SUPPORTED_OPTS = ['all'];
+
 export default async function add(git: Git, opts: Opts = {}): Promise<string> {
+    checkSupportedOpts('add', opts, SUPPORTED_OPTS);
+
     const args = ['add', mapOpts(opts)];
     const out = await git(args);
     return out;
