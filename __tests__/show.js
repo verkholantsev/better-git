7169e9d3632e9d3f242ea91b61f0a007200d8d0e
Aleksei Verkholantcev <averkholantcev@atlassian.com>
2018-03-29 21:20:06 +1100

    Add test to `show`

--- __tests__/show.js
+++ __tests__/show.js
@@ -9,10 +9,19 @@ import show from '../src/show';
 const readFile = promisify(fs.readFile);
 
 describe('git.show()', () => {
-    it('should work correctly', async () => {
-        const fixturePath = path.resolve(__dirname, '__fixtures__', 'git-show');
-        const git = async () => await readFile(fixturePath, { encoding: 'utf8' });
+    let git;
 
+    beforeEach(() => {
+        const fixturePath = path.resolve(__dirname, '__fixtures__', 'git-show');
+        git = jest.fn(async () => await readFile(fixturePath, { encoding: 'utf8' }));
+    });
+
+    it('should produce correct output', async () => {
         expect(await show(git)).toMatchSnapshot();
+    });
+
+    it('should call git with correct args', async () => {
+        await show(git);
+        expect(git).toBeCalledWith(['show']);
     });
 });
